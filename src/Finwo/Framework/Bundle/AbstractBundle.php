fbf87a98efd5f1b10d4b821d4b5d30883b6ebc1c
Robin Bron <robin@finwo.nl>
2017-02-22 22:42:31 +0100

    Bundle & service registering ; started on controller registering

--- src/Finwo/Framework/Bundle/AbstractBundle.php
+++ src/Finwo/Framework/Bundle/AbstractBundle.php
@@ -2,7 +2,25 @@
 
 namespace Finwo\Framework\Bundle;
 
+use Klein\Klein;
+
 abstract class AbstractBundle
 {
-
+  public function __construct( Klein $router )
+  {
+    // Fetch info
+    $class_info     = new \ReflectionClass($this);
+    $path           = dirname($class_info->getFileName());
+    $controllerPath = $path . DS . 'Controller';
+    
+    // Fetch controllers
+    $controllers = array();
+    foreach( glob($controllerPath.DS.'*.php') as $file ) {
+      $class = explode("\\", get_class($this));
+      array_pop($class);
+      $class = implode("\\", $class) . "\\Controller\\" . @array_shift(explode('.',array_pop(explode(DS,$file))));
+      if(!class_exists($class)) continue;
+      die('Registering controller: '.$class);
+    }
+  }
 }
---
+++ src/Finwo/Framework/Service/AbstractService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace \Finwo\Framework\Service;
+
+abstract class AbstractService
+{
+  
+}
