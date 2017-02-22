036245530cf65c9d7bdb5fe34693bce1ea9a769b
Robin Bron <robin@finwo.nl>
2017-02-22 11:29:09 +0100

    HTTP working ; main dependencies selected

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-XRARGS= -x -v -s http:0.0.0.0:$PORT
+XRARGS="-x -s http:0.0.0.0:$PORT"
+rm fifo.* 2>/dev/null
 
 for i in {0..9} ; do
-  XRARGS="$XARGS -b 127.0.0.1:800$i"
-  while true ; do FIFO=$(hexdump -n 16 -v -e '/1 "%02X"' -e '/16 "\n"' /dev/urandom) ; mkfifo fifo.$FIFO ; nc -l 800$i < fifo.$FIFO | php sgi.php > fifo.$FIFO ; rm fifo.$FIFO ; done &
+  XRARGS="$XRARGS -b 127.0.0.1:800$i"
+  echo Starting handler: 800$i
+  { while true ; do FIFO=$(hexdump -n 16 -v -e '/1 "%02X"' -e '/16 "\n"' /dev/urandom) ; mkfifo fifo.$FIFO ; nc -l 800$i < fifo.$FIFO | php init.php > fifo.$FIFO ; rm fifo.$FIFO ; done } &
 done;
 
+echo Starting load-balancer on $PORT
+echo xr $XRARGS
 xr $XRARGS
